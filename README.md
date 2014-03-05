82a2f6a9af3df921f6db2768908e941591ba999c
pR0Ps <pR0Ps.CM@gmail.com>
2014-03-05 00:05:25 -0500

    Initial commit - quick hack, no version history

---
+++ README.md
@@ -0,0 +1,53 @@
+# addtask
+
+## Description
+[Addtask](https://github.com/pr0ps/addtask) is a command-line utility to add tasks to Google Tasks. It understands most natural language queries (just don't get too crazy).
+For example `./addtask.py 1 month, 2 weeks, and 3 days from now do something`.
+
+I built this because I was frustrated with the lack of tools to quickly add tasks.
+Other tools all enforce a stricter command syntax.
+I didn't want to think about date formatting or anything, I just wanted to create the task.
+
+## Limitations
+Currenty nothing is implemented to split up the date and the title of the task.
+Because of this the task will be added with the name exactly as typed.
+For example, the command above will create an event called "1 month, 2 weeks, and 3 days from now do something" in 1 month, 2 weeks, and 3 days.
+
+## Dependencies
+Requires Python 2.7 and the [Google API client for Python](http://code.google.com/p/google-api-python-client/).
+The needed packages are installable from PyPi and are in the `requirements.txt` file. Use `pip install -r requirements.txt` to install them.
+
+**NOTE**: This script will create an unencypted `~/.addtask/keys.txt` to hold API credentials on disk.
+
+## Installation
+
+### Connecting to the Tasks API
+1. Go to https://cloud.google.com/console/project
+2. Create a project.
+3. Enable the Tasks API (disable everything else).
+4. Go to the credentials screen.
+5. Click the "Create new Key" button under the Public API access section.
+6. Generate a browser key and leave the URL restriction blank.
+7. Install the requirements in `requirements.txt`
+7. Run addtask and enter the needed information.
+
+### Local
+
+Symlink or move the script into a folder in your $PATH. Feel free to take off the '.py' extension as well
+      
+## Usage Examples
+
+      addtask Do groceries tomorrow
+      addtask Buy a birthday card next week   
+      addtask Buy a drink this friday
+      addtask Buy a drink next friday
+      addtask Essay due in a week
+      addtask Apply for job on saturday
+      addtask Pay bills next month
+      addtask TPS report in 3 days
+      addtask Remember Birthday April 17th
+
+## Development/License
+Addtask uses authentication code from [Tasky by Ajay Roopakalu](https://github.com/jrupac/tasky) and is therefore licenced under the [GNU GPL license](http://www.gnu.org/licenses/gpl.txt).
+
+Bug reports and pull requests welcome!
---
+++ addtask.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python2
+
+from apiclient.discovery import build
+from argparse import ArgumentParser
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.file import Storage
+from oauth2client.tools import run_flow
+import httplib2
+
+import parsedatetime
+from datetime import date
+import calendar
+import os
+
+tasky_dir = os.path.join(os.environ['HOME'], '.config', 'tasky')
+KEYS_FILE = os.path.join(tasky_dir, 'keys.txt')
+OAUTH_FILE = os.path.join(tasky_dir, 'oauth.dat')
+
+
+class Auth():
+    def __init__(self, key_file):
+        try:
+            with open(key_file, 'r') as f:
+                self.clientid = f.readline().strip()
+                self.clientsecret = f.readline().strip()
+                self.apikey = f.readline().strip()
+        except IOError:
+            self.clientid = raw_input("Enter your clientID: ")
+            self.clientsecret = raw_input("Enter your client secret: ")
+            self.apikey = raw_input("Enter your API key: ")
+            self.write_auth()
+
+    def write_auth(self):
+        if not os.path.exists(tasky_dir):
+            os.makedirs(tasky_dir)
+        with open(KEYS_FILE, 'w') as auth:
+            auth.write(str(self.clientid) + '\n')
+            auth.write(str(self.clientsecret) + '\n')
+            auth.write(str(self.apikey) + '\n')
+
+
+def add_task(service, title, due):
+    try:
+        ret = service.tasks().insert(tasklist="@default", body=dict(title=title, due=due)).execute()
+        print ("Added task '{0}'".format(ret["title"]))
+    except (EnvironmentError, TypeError) as e:
+        print ("Error adding task: {0}".format(str(e)))
+
+
+def make_parser():
+    parser = ArgumentParser(description = """Add to Google Tasks with natural language""")
+
+    parser.add_argument('strs', metavar='string', type=str, nargs='*',
+        help='Can be something like "Pick up milk today"')
+
+    key_exist = os.path.exists(KEYS_FILE)
+    auth_exist = os.path.exists(OAUTH_FILE)
+
+    if not key_exist or not auth_exist:
+        # Required by oauth2client
+        parser.add_argument('--auth_host_name', default='localhost',
+                       help='Hostname when running a local web server.')
+        parser.add_argument('--noauth_local_webserver', action='store_true',
+                       default=False, help='Do not run a local web server.')
+        parser.add_argument('--auth_host_port', default=[8080, 8090], type=int,
+                       nargs='*', help='Port web server should listen on.')
+        parser.add_argument('--logging_level', default='ERROR',
+                       choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                       help='Set the logging level of detail.')
+    return parser
+
+
+def authenticate(args):
+    f = Auth(KEYS_FILE)
+
+    storage = Storage(OAUTH_FILE)
+    credentials = storage.get()
+
+    if credentials is None or credentials.invalid:
+        # OAuth 2.0 Authentication
+        FLOW = OAuth2WebServerFlow(
+            client_id=f.clientid,
+            client_secret=f.clientsecret,
+            scope='https://www.googleapis.com/auth/tasks',
+            user_agent='Tasky/v1')
+
+        credentials = run_flow(FLOW, storage, args)
+
+    http = httplib2.Http()
+    http = credentials.authorize(http)
+
+    # The main Tasks API object
+    return build(serviceName='tasks', version='v1', http=http, developerKey=f.apikey)
+
+
+def parse(args):
+    command = " ".join(args.strs)
+    cal = parsedatetime.Calendar()
+    datetime, kind = cal.parse(command)
+    dt = date.fromtimestamp(calendar.timegm(datetime))
+
+    if not kind:
+        print ("Couldn't parse out a date, using today")
+    else:
+        print ("Parsed date: {0}".format(dt.strftime("%a %b %d %Y")))
+
+    return command, dt.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+
+def main(service, args):
+
+    if len(args.strs) < 1:
+        args.print_help()
+        return
+
+    title, date = parse(args)
+    add_task(service, title, date)
+
+
+if __name__ == '__main__':
+    parser = make_parser()
+    args = parser.parse_args()
+    service = authenticate(args)
+    if len(args.strs) <1:
+        parser.print_help()
+    else:
+        main(service, args)
